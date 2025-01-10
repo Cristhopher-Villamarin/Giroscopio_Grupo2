77592579d0a28f9cabbc0aab58260ab166ad646c
Jcnazate11 <87491197+Jcnazate11@users.noreply.github.com>
2025-01-10 11:51:41 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,21 +2,26 @@ import asyncio
 import websockets
 import webbrowser
 
-async def handler(websocket, path):
+async def handler(websocket):
     print(f"Cliente conectado: {websocket.remote_address}")
     async for message in websocket:
         print(f"Mensaje recibido: {message}")
         if message == "open_web":
-            webbrowser.open('https://refactoring.guru/es/design-patterns')
+            webbrowser.open('https://www.google.com/?hl=es')
         elif message == "open_sheet":
-            webbrowser.open('https://docs.google.com/spreadsheets/d/0B7uNrThIIZTmeWZka2ZvemUtYkE/edit?gid=1307076623&resourcekey=0-2Be8unJoKd74W_jnqGKNaQ#gid=1307076623')
+            webbrowser.open('https://docs.google.com/document/d/1WM3TNf-1JgdyRRVZQUH6Wh_KVj0wbednESM34saASms/edit?tab=t.0')
         elif message == "open_whatsapp":
-            webbrowser.open('https://web.whatsapp.com')
+            webbrowser.open('https://web.whatsapp.com/')
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", 8765):
-        print("Servidor WebSocket escuchando en ws://0.0.0.0:8765")
-        await asyncio.Future()  # run forever
+    print("Iniciando el servidor WebSocket...")
+    try:
+        async with websockets.serve(handler, "0.0.0.0", 8080):
+            print("Servidor WebSocket escuchando en ws://0.0.0.0:8080")
+            await asyncio.Future()  # Mantiene el servidor corriendo
+    except Exception as e:
+        print(f"Error al iniciar el servidor: {e}")
+  # run forever
 
 if __name__ == "__main__":
     asyncio.run(main())
